87fa510657fb0170d6b09ef194780d8fb3352802
eyedi <gdregas18@gmail.com>
2020-06-10 09:35:35 +0900

    [master/branche]
    > Solve problem which is triplets
    > Start problem is Magic-square

--- comp-triplets/triplets.kt
+++ comp-triplets/triplets.kt
@@ -19,17 +19,30 @@ import kotlin.text.*
 
 // Complete the compareTriplets function below.
 fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
+	var alice: Int = 0
+	var bob: Int = 0
+	var c : Array<Int> = arrayOf(0,0)
 
+	for(i in 0..2){
+		var sub : Int = a[i] - b[i]
+		
+		if (sub > 0)
+			alice++
+		else if (sub < 0)
+			bob++
+	}
 
+	c.set(0, alice)
+	c.set(1, bob)
+
+	return c
 }
 
 fun main(args: Array<String>) {
-	    val a = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    val a = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    val b = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
 
-		    val b = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
-
-			    val result = compareTriplets(a, b)
-
-				    println(result.joinToString(" "))
+	val result = compareTriplets(a, b)
+	println(result.joinToString(" "))
 }
 
