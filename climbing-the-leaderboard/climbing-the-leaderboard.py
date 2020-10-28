0d2de7fda5cd16e5908803a8cd07fbd65e8786d4
eyedi <gdregas18@gmail.com>
2020-10-28 19:02:32 +0900

    [master]
    1. Analysis to source code which is sovled this problem
    2. Add to A-* algorithm directory

--- climbing-the-leaderboard/climbing-the-leaderboard.py
+++ climbing-the-leaderboard/climbing-the-leaderboard.py
@@ -1,3 +1,5 @@
+#!/bin/python3
+
 import math
 import os
 import random
@@ -5,27 +7,35 @@ import re
 import sys
 
 # Complete the climbingLeaderboard function below.
+"""
 def climbingLeaderboard(scores, alice):
     result = []
-    start_score = alice[0]
-    before_score = scores[0]
-    rank = 1
-    index = 0
 
-    # find index, rank
-    for idx, score in enumerate(scores):
-        if (start_score < score):
-            if (before_score != score):
-                rank = rank + 1
-                before_score = score
-                index = idx
+    for a_score in alice:
+        rank = 1 
+        # Sorting socres array
+        scores.sort(reverse = True)
+        alice.sort()
+        before_score = scores[0]
 
-    print("index: " + str(index))
-    print("rank : " + str(rank))
+        for idx, score in enumerate(scores):
+            if a_score < score:
+                if before_score != score:
+                    before_score = score;
+                    rank = rank + 1 
+                else :
+                    if idx != 0:
+                         rank = rank + 1 
+                         result.append(rank)
+                    break
+
+                if (idx + 1) == int(len(scores)):
+                    rank = rank + 1 
+                    result.append(rank)
 
     return result
 
-
+# Main function
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
@@ -41,6 +51,33 @@ if __name__ == '__main__':
 
     fptr.write('\n'.join(map(str, result)))
     fptr.write('\n')
-
     fptr.close()
+"""
+
+#!/bin/python3
+# Can't solved this problem. 
+# So, need to Analyze to source code which is uploaded at HackerRank
+n = int(input().strip())
+scores_raw = [int(scores_temp) for scores_temp in input().strip().split(' ')]
+scores_list = list(set(scores_raw))
+scores = list(reversed(sorted(scores_list)))
+m = int(input().strip())
+alice = [int(alice_temp) for alice_temp in input().strip().split(' ')]
+## Start debug
+print(alice)
+#print(scores_raw)
+# Deleted to duplicated number
+#print(scores_list)
+#print(sorted(scores_list))
+#print(reversed(sorted(scores_list)))
+print(list(reversed(sorted(scores_list))))
+# End debug 
+
+for x in alice:
+#print(x)
+    while len(scores) > 0 and scores[-1] <= x:
+#print(scores)
+        del scores[-1]
+# if duplicated number deleted, scores length + 1 is to be rank
+    print(len(scores) + 1)
 
