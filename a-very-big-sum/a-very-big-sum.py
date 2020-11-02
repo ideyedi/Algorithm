baae91ae9fc766b340c03b85198f4209c1c87843
eyedi <gdregas18@gmail.com>
2020-11-02 20:01:25 +0900

    * branch/master
    Add to algor problem which is a-big-sum 'a very easy' & 'WTF'

---
+++ a-very-big-sum/a-very-big-sum.py
@@ -0,0 +1,26 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    big_sum = 0
+    for item in ar:
+        big_sum += item
+
+    return big_sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
