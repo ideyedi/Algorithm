688179133d18aa36e4d478fc4b9b5526bbcf2428
eyedi <gdregas18@gmail.com>
2021-03-08 07:18:54 +0900

    [master] hanyang uni python programming 1st l:q :ecture

---
+++ hanyang-python/21.03.06.1st/ex5.py
@@ -0,0 +1,13 @@
+#/bin/python3
+# practice problem 5
+
+chip_bugger = 0
+chip_baver = 0
+h_bugger = int(input())
+m_bugger = int(input())
+l_bugger = int(input())
+coke = int(input())
+sidar = int(input())
+
+if h_bugger > chip_buffer:
+
