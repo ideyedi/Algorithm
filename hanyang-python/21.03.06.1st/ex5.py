939953c714c23b76093089e24d47fbb62c06b7a8
Eunseok Ji <gdregas18@gmail.com>
2021-03-12 17:16:41 +0900

    [master/branches] 1. solve the problem which is exercise num.5

--- hanyang-python/21.03.06.1st/ex5.py
+++ hanyang-python/21.03.06.1st/ex5.py
@@ -9,5 +9,20 @@ l_bugger = int(input())
 coke = int(input())
 sidar = int(input())
 
-if h_bugger > chip_buffer:
+chip_bavar = 0
+chip_bugger = h_bugger
 
+if m_bugger < chip_bugger:
+	chip_bugger = m_bugger
+
+if l_bugger < chip_bugger:
+	chip_bugger = l_bugger
+
+if coke > sidar:
+	chip_bavar = sidar
+else:
+	chip_bavar = coke
+
+
+print("low set price")
+print(chip_bugger + chip_bavar)
