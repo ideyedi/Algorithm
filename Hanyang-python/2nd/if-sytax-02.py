cf98c8d5befc504aa94efdc089eef8e4b237bc08
지은석 <esji@wemakeprice.com>
2021-08-27 12:09:42 +0900

    branch/main
    seperate repo which is algorithm to hanyang-programming lecture

--- Hanyang-python/2nd/if-sytax-02.py
+++
@@ -1,6 +0,0 @@
-emails = ['taehwa@gmail.com', 'dlxoghk@gmail.com', 'yongseong']
-desc = 'For developer'
-students_count = 10
-
-if 'developer' in desc:
-    print(desc.replace('developer', 'beginner'))
