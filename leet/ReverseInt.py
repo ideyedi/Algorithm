2036ad35b79a8c2f5ede1396df5712932993572a
esji <esji@wemakeprice.com>
2021-09-28 17:37:23 +0900

    branch main
    PS that reverse integer at leet code

--- leet/ReverseInt.py
+++ leet/ReverseInt.py
@@ -1,11 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x > 0:
-            print('양수')
-        else:
-            print('dmatn)'
+        value = 0
 
-        return x
+        if x > 0:
+            value = int(str(x)[::-1])
+        else:
+            value = (-1) * int(str(x*(-1))[::-1])
+
+        if value not in range(-2**31, 2**31):
+            value = 0
+
+        return value 
 
 sol = Solution()
 x = 10
