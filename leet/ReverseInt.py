0c36953cf7b0e9c4439dab693cfbe5d94bc2e9a8
esji <esji@wemakeprice.com>
2021-09-28 16:41:24 +0900

    branch/main
    1. Fix python version 3.9.6
    2. commit leet problem which is Reverse Integer

---
+++ leet/ReverseInt.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x > 0:
+            print('양수')
+        else:
+            print('dmatn)'
+
+        return x
+
+sol = Solution()
+x = 10
+print(sol.reverse(x))
