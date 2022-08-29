7a1477c064ee2cb907e7bbfef994f8b061d535db
ideyedi <gdregas18@gmail.com>
2022-08-29 20:59:54 +0900

    임시 커밋

---
+++ leet/esji/Reverse_Nodes_in_kGroup.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        return 0
+
+
+if __name__ == "__main__":
