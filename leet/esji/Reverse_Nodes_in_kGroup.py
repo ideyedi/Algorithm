0e49e369ce64f00705ec2fdcc9ca2105f704397a
ideyedi <gdregas18@gmail.com>
2022-08-30 12:35:33 +0900

    [branch esji]
    1. gitignore python project 관련 값 전부 적용
    2. 테스트 템플릿 통합

--- leet/esji/Reverse_Nodes_in_kGroup.py
+++ leet/esji/Reverse_Nodes_in_kGroup.py
@@ -1,15 +1,26 @@
 from typing import Optional
 
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
+        self.val = val
+        self.next = next
 
 
 class Solution:
+    def __init__(self):
+        pass
+
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        return 0
+        print(head.val)
+        print(head.next)
+
+    def print(self):
+        pass
 
 
 if __name__ == "__main__":
+    linked_list = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+    print(linked_list.val)
+    print(linked_list.next)
