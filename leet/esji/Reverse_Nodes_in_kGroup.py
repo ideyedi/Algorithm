6c098a671b350069f31912a0e970c0f212efea04
ideyedi <gdregas18@gmail.com>
2022-08-30 17:35:27 +0900

    template commit

--- leet/esji/Reverse_Nodes_in_kGroup.py
+++ leet/esji/Reverse_Nodes_in_kGroup.py
@@ -12,15 +12,43 @@ class Solution:
     def __init__(self):
         pass
 
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        print(head.val)
-        print(head.next)
+    def __reversed(self):
+        """
+        Private method
+        :return: reversed linked list
+        """
+        return 0
 
-    def print(self):
-        pass
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        """
+        :param head: input linked list, range 0 <= head.val <= 1000
+        :param k: Range 1 <= k <= n <= 5000
+            n: The number of nodes in the list
+        :return: reversed linked list
+        """
+        if k == 1 or not head:
+            return head
+
+        index = 0
+        while head.next is not None:
+            return head
+
+
+def print_linked_list(nodes):
+    """
+    :param nodes: 입력된 링크드 리스트 내용을 확인을 위한 출력 함수
+    :return: 리스트 값이 없을 때까지 해당 노드의 값을 출력
+    """
+    while nodes is not None:
+        print(f'{nodes.val}', end="->")
+        nodes = nodes.next
+    print('\n'+'-'*20)
 
 
 if __name__ == "__main__":
     linked_list = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
-    print(linked_list.val)
-    print(linked_list.next)
+    obj = Solution()
+
+    print_linked_list(linked_list)
+    linked_list = obj.reverseKGroup(linked_list, 1)
+    print_linked_list(linked_list)
