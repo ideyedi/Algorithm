5380a2306d325ea76512e1f40036e588efc86a91
ideyedi <gdregas18@gmail.com>
2022-08-31 14:09:33 +0900

    진행내역 커밋

--- leet/esji/Reverse_Nodes_in_kGroup.py
+++ leet/esji/Reverse_Nodes_in_kGroup.py
@@ -29,9 +29,38 @@ class Solution:
         if k == 1 or not head:
             return head
 
-        index = 0
-        while head.next is not None:
-            return head
+        swap_node = ListNode(-1, head)
+
+        prev = swap_node
+        curr = swap_node
+        next_node = swap_node
+        count = 0
+
+        # Linked List counting
+        # Time Complexity: O(n)
+        while curr:
+            curr = curr.next
+            count += 1
+
+        count -= 1
+        # next node가 NoneType일 경우 순회를 종료
+        while next_node:
+            curr = prev.next
+            next_node = curr.next
+
+            to_loop = count > k and k or count - 1
+
+            for _ in range(1, to_loop):
+                curr.next = next_node.next
+                next_node.next = prev.next
+
+                prev.next = next_node
+                next_node = curr.next
+
+            prev = curr
+            count -= k
+
+        return swap_node.next
 
 
 def print_linked_list(nodes):
@@ -50,5 +79,5 @@ if __name__ == "__main__":
     obj = Solution()
 
     print_linked_list(linked_list)
-    linked_list = obj.reverseKGroup(linked_list, 1)
+    linked_list = obj.reverseKGroup(linked_list, 3)
     print_linked_list(linked_list)
