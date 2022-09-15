6de03644adbbe195b82c08220103646134ab42ed
ideyedi <gdregas18@gmail.com>
2022-09-15 15:15:03 +0900

    02 문제 템플릿 추가 - 쉬어가기

---
+++ leet/esji/2.Add_Two_Numbers.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        pass
+
+
+if __name__ == "__main__":
+    l1 = ListNode()
+    l2 = ListNode()
+    sol = Solution()
+
+
+
