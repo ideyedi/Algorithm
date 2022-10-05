8b6799e470cb630d612b2baa1af62a6d07a5dda1
ideyedi <gdregas18@gmail.com>
2022-10-05 12:33:45 +0900

    마지막 노드만 안남게 수정하면 될듯?

--- leet/esji/02.Add_Two_Numbers.py
+++ leet/esji/02.Add_Two_Numbers.py
@@ -1,3 +1,4 @@
+#! python
 from typing import Optional
 
 
@@ -14,29 +15,40 @@ class Solution:
 
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         sum_list = ListNode()
+        curr = sum_list
         upper = 0
 
         while l1 and l2:
-            print(l1.val, l2.val)
-            s = l1.val + l2.val + upper
-            if s < 10:
-                sum_list.val = s
-            else:
-                sum_list.val = s - 10
-                upper = 1
-
-
+            # 10 이상으로 합이 넘어 온 경우 먼저 처리해야함
+            s = l1.val + l2.val + upper 
+            upper = s // 10
+            curr.val = s % 10
+            
+            curr.next = ListNode()
+            curr = curr.next
             l1 = l1.next
-            l2 = l2.next
+            l2 = l2.next 
 
         return sum_list
 
 
+def print_linked_list(nodes):
+    """
+    :param nodes: 입력된 링크드 리스트 내용을 확인을 위한 출력 함수
+    :return: 리스트 값이 없을 때까지 해당 노드의 값을 출력
+    """
+    while nodes is not None:
+        print(f'{nodes.val}', end="->")
+        nodes = nodes.next
+    print('\n'+'-'*20)
+
+
 if __name__ == "__main__":
-    l1 = ListNode(2, ListNode(4, ListNode(3, ListNode(5))))
+    l1 = ListNode(2, ListNode(4, ListNode(3)))
     l2 = ListNode(5, ListNode(6, ListNode(4)))
     sol = Solution()
-    print(sol.addTwoNumbers(l1, l2))
+    #print(sol.addTwoNumbers(l1, l2))
+    print_linked_list(sol.addTwoNumbers(l1, l2))
 
 
 
