6bf39ccbb2e5294872a119315ee9db02a79fff09
ideyedi <gdregas18@gmail.com>
2023-02-01 17:55:46 +0900

    lists length가 10^4까지 가능해서 다른 방법을 찾아야 하나..?

--- leet/esji/23.merge_k_sorted_lists.py
+++ leet/esji/23.merge_k_sorted_lists.py
@@ -11,16 +11,45 @@ class ListNode:
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        pass
+        ret = ListNode()
+        dummy = ret
+        curr = ret
+
+        while len(lists) != 0:
+            print(lists, len(lists))
+            tmp = []
+
+            for ls in lists:
+                tmp.append(ls.val)
+
+            m = min(tmp)
+            curr.val = m
+            curr.next = ListNode()
+            curr = curr.next
+
+            i = tmp.index(m)
+            lists[i] = lists[i].next
+            if lists[i] is None:
+                lists.remove(lists[i])
+
+        return ret
 
     def print_list_node(self, nodes):
         while nodes is not None:
             print(f"{nodes.val}", end="-")
             nodes = nodes.next
 
+        print("")
+
 
 if __name__ == "__main__":
     list1 = ListNode(1, ListNode(4, ListNode(5)))
+    list2 = ListNode(1, ListNode(3, ListNode(4)))
+    list3 = ListNode(2, ListNode(6))
+
+    lists = [list1, list2, list3]
     ins = Solution()
 
-    ins.print_list_node(list1)
+    ret = ins.mergeKLists(lists)
+    ins.print_list_node(ret)
+    #ins.print_list_node(list1)
