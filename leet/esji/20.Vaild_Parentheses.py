36f3e62740fd7f462b7b342975b43df9bd4573b6
ideyedi <gdregas18@gmail.com>
2023-03-13 16:04:55 +0900

    다음 풀 문제 20 번!!

---
+++ leet/esji/20.Vaild_Parentheses.py
@@ -0,0 +1,16 @@
+#! python
+from typing import List
+
+
+class Solution:
+    start_brackets: List[str] = ['(', '[', '{']
+
+    def isValid(self, s: str) -> bool:
+        ret = False
+
+        return ret
+
+
+if __name__ == "__main__":
+    s: str = "()"
+    #s: str = "{[}]"
