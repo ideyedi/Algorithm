b915860496ec4e230ed1994fd8db4ff6df5e3985
ideyedi <gdregas18@gmail.com>
2023-04-19 11:50:46 +0900

    1768번 Go로 풀어보기, 이제 Golang 공부 간닷

--- leet/esji/1768.Merge_Strings_Alternately.go
+++ leet/esji/1768.Merge_Strings_Alternately.go
@@ -5,9 +5,25 @@ import (
 )
 
 func mergeAlternately(word1 string, word2 string) string {
-	return "OK"
+	var answer string = ""
+	var index int = 0
+
+	for {
+		if len(word1) == index {
+			answer += string(word2[index:])
+			break
+		} else if len(word2) == index {
+			answer += string(word1[index:])
+			break
+		} else {
+			fmt.Println(word1[index] + word2[index])
+			answer += string(word1[index]) + string(word2[index])
+		}
+		index += 1
+	}
+	return answer
 }
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(mergeAlternately("ab", "qwe"))
 }
